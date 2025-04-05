0289e674160b505814744dd50a9118b7311aa770
meera37 <meerabenny370@gmail.com>
2025-04-05 11:29:33 +0530

    Home added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Landing  from "./components/Landing";
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
       <Header/>
+      <Home/>
       <Landing/>
       <Footer/>
     </>
